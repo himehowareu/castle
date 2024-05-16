90fcc69dc58d100185bf604a84cb8b359b81d006
Himehowareu <himehowareu@gmail.com>
2024-05-15 21:54:54 -0500

    added some network stuff and change dic for args to ;

---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+use base64::{engine::general_purpose, Engine as _};
+use mlua::Lua;
+use regex::Regex;
+use shell_words;
+use std::fmt::format;
+use std::fs::{self, File};
+use std::io::Read;
+use std::{
+    env,
+    path::Path,
+    process::{exit, Command},
+};
+use tera::Context;
+use tera::Tera;
+extern crate clap;
+
+pub enum RenderType {
+    Simple,
+    Complex,
+}
+
+pub fn run(_: &str, command: &&str) -> String {
+    let a = shell_words::split(command);
+    let mut b = a.unwrap();
+    let mut out;
+    if env::consts::OS == "windows" {
+        let e = Command::new("cmd")
+            .args(["/c", "echo", "error with command"])
+            .output()
+            .unwrap();
+        let output = Command::new("cmd").arg("/c").args(b).output().unwrap_or(e);
+        out = String::from_utf8_lossy(&output.stdout).to_string();
+        if out.len() < 1 {
+            out = String::from_utf8_lossy(&output.stderr).to_string();
+        }
+    } else {
+        let e = Command::new("echo")
+            .arg("error with command")
+            .output()
+            .unwrap();
+        let output = Command::new(b.remove(0)).args(b).output().unwrap_or(e);
+        out = String::from_utf8_lossy(&output.stdout).to_string();
+        if out.len() < 1 {
+            out = String::from_utf8_lossy(&output.stderr).to_string();
+        }
+    }
+    rem_last(out)
+}
+
+pub fn rem_last(value: String) -> String {
+    let mut chars = value.chars();
+    chars.next_back();
+    chars.as_str().to_string()
+}
+
+pub fn run_lua(code: &str, _: &&str) -> String {
+    let lua = Lua::new();
+    lua.load(code)
+        .eval::<String>()
+        .unwrap_or("lua code errored".to_string())
+}
+
+pub fn try_read_file(file_path: &str) -> String {
+    if !Path::new(file_path).exists() {
+        println!("target file {file_path} does not exist");
+        exit(-1);
+    } else {
+        let mut file = File::open(file_path).unwrap();
+        let mut code = String::new();
+        file.read_to_string(&mut code).unwrap();
+        code
+    }
+}
+
+pub fn run_macro(file_path: &str, args: &&str) -> String {
+    let code = try_read_file(file_path);
+
+    let lua = Lua::new();
+    let globals = lua.globals();
+    globals.set("args", args.to_string()).unwrap();
+    lua.load(code).exec().unwrap();
+    globals
+        .get::<_, String>("output")
+        .unwrap_or("error in loaded lua code : not output varible defined".into())
+}
+
+pub fn get_json(settings_file: &str, setting: &&str) -> String {
+    let content = try_read_file(settings_file);
+
+    let json: serde_json::Value = serde_json::from_str(&content).unwrap();
+    let first_name = json.get(setting).unwrap();
+
+    first_name.to_string()
+}
+
+pub fn file_to_base64(_: &str, file_path: &&str) -> String {
+    let contents = try_read_file(file_path);
+    let encoded = general_purpose::STANDARD.encode(contents);
+    encoded
+}
+
+pub fn minify(test: String) -> String {
+    test.replace("\n", "").replace("\'", "\"")
+}
+
+pub fn download_file(url: &str, file_path: &&str)->String {
+    let resp = reqwest::blocking::get(url).expect("request failed");
+    let body = resp.bytes().expect("body invalid");
+    let _ = std::fs::write(file_path, &body);
+    format!("Downloaded {file_path}")
+}
+
+pub fn get_file(_: &str, url: &&str) -> String {
+    let resp = reqwest::blocking::get(url.to_string()).expect("request failed");
+    let body = resp.text().expect("body invalid");
+    body
+}
+
+pub fn delete(_:&str,file_path:&&str)->String{
+    fs::remove_file(file_path).unwrap();
+    format!("deleted file {file_path}")
+}
+
+pub fn delete_folder(_:&str,file_path:&&str)->String{
+    fs::remove_dir_all(file_path).unwrap();
+    format!("deleted folder {file_path}")
+}
+
+pub fn download_git(file_path: &str, args: &&str) -> String {
+    let _ = Command::new("git")
+                     .args(&["clone", file_path,args])
+                     .output()
+                     .expect("Failed to execute command");
+
+    // println!("git {file_path} downloaded and saved to {args} ");
+    format!("git {file_path} downloaded and saved to {args} ")
+}
+
+pub fn run_blueprint(file_path: &str, args: &&str) -> String {
+    let contents = try_read_file(file_path);
+
+    let mut context = Context::new();
+    for arg in shell_words::split(args).unwrap() {
+        let sep = arg.split("=").collect::<Vec<&str>>();
+        context.insert(sep[0], sep[1])
+    }
+
+    let result = Tera::one_off(contents.as_str(), &context, true);
+
+    result.unwrap_or("error in temple".to_string())
+}
+
+pub fn render_with(
+    text: String,
+    select: Regex,
+    renderer: fn(&str, &&str) -> String,
+    split_type: RenderType,
+) -> String {
+    let mut target_text = text.clone();
+    for selected in select
+        .captures_iter(target_text.clone().as_str())
+        .map(|c| c.extract::<1>())
+    {
+        let found = selected.0;
+        let command = &selected.1[0];
+        let include_text: String;
+        match split_type {
+            RenderType::Complex => {
+                let sep = command.split(";").collect::<Vec<&str>>();
+                let file_name = sep[0];
+                let mut args = "";
+                if sep.len() > 1 {
+                    args = sep[1];
+                }
+                include_text = renderer(file_name, &args);
+            }
+            RenderType::Simple => {
+                include_text = renderer(found, &command);
+            }
+        }
+        target_text = target_text.replace(found, &include_text);
+    }
+    target_text
+}
--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,11 @@
-use base64::{engine::general_purpose, Engine as _};
-use mlua::Lua;
 use regex::Regex;
-use shell_words;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::{
-    env,
-    path::Path,
-    process::{exit, Command},
-};
-use tera::Context;
-use tera::Tera;
+use std::io::Write;
+use std::process::exit;
 extern crate clap;
+use castle::*;
 
-fn run(_: &str, command: &&str) -> String {
-    let a = shell_words::split(command);
-    let mut b = a.unwrap();
-    let mut out;
-    if env::consts::OS == "windows" {
-        let e = Command::new("cmd")
-            .args(["/c", "echo", "error with command"])
-            .output()
-            .unwrap();
-        let output = Command::new("cmd").arg("/c").args(b).output().unwrap_or(e);
-        out = String::from_utf8_lossy(&output.stdout).to_string();
-        if out.len() < 1 {
-            out = String::from_utf8_lossy(&output.stderr).to_string();
-        }
-    } else {
-        let e = Command::new("echo")
-            .arg("error with command")
-            .output()
-            .unwrap();
-        let output = Command::new(b.remove(0)).args(b).output().unwrap_or(e);
-        out = String::from_utf8_lossy(&output.stdout).to_string();
-        if out.len() < 1 {
-            out = String::from_utf8_lossy(&output.stderr).to_string();
-        }
-    }
-    rem_last(out)
-}
 
-fn run_lua(code: &str) -> String {
-    let lua = Lua::new();
-    lua.load(code)
-        .eval::<String>()
-        .unwrap_or("lua code errored".to_string())
-}
-
-fn try_read_file(file_path: &str) -> String {
-    if !Path::new(file_path).exists() {
-        println!("target file {file_path} does not exist");
-        exit(-1);
-    } else {
-        let mut file = File::open(file_path).unwrap();
-        let mut code = String::new();
-        file.read_to_string(&mut code).unwrap();
-        code
-    }
-}
-
-fn run_macro(file_path: &str, args: &str) -> String {
-    let code = try_read_file(file_path);
-
-    let lua = Lua::new();
-    let globals = lua.globals();
-    globals.set("args", args).unwrap();
-    lua.load(code).exec().unwrap();
-    globals
-        .get::<_, String>("output")
-        .unwrap_or("error in loaded lua code : not output varible defined".into())
-}
-
-fn get_json(settings_file: &str, setting: &str) -> String {
-    let content = try_read_file(settings_file);
-
-    let json: serde_json::Value = serde_json::from_str(&content).unwrap();
-    let first_name = json.get(setting).unwrap();
-
-    first_name.to_string()
-}
-
-fn file_to_base64(file_path: &str) -> String {
-    let contents = try_read_file(file_path);
-    let encoded = general_purpose::STANDARD.encode(contents);
-    encoded
-}
-
-fn minify(test: String) -> String {
-    test.replace("\n", "").replace("\'", "\"")
-}
-
-fn run_blueprint(file_path: &str, args: &str) -> String {
-    let contents = try_read_file(file_path);
-
-    let mut context = Context::new();
-    for arg in shell_words::split(args).unwrap() {
-        let sep = arg.split("=").collect::<Vec<&str>>();
-        context.insert(sep[0], sep[1])
-    }
-
-    let result = Tera::one_off(contents.as_str(), &context, true);
-
-    result.unwrap_or("error in temple".to_string())
-}
-
-fn render_with(text: String, select: Regex, renderer:fn(&str, &&str) -> String) -> String {
-    let mut target_text = text.clone();
-    for selected in select
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = selected.0;
-        let command = &selected.1[0];
-        let include_text = renderer(found, &command);
-        target_text = target_text.replace(found, &include_text);
-    }
-    target_text
-}
 
 fn render(target: &str, files: Vec<&str>) -> String {
     let mut target_text = try_read_file(target);
@@ -129,6 +17,11 @@ fn render(target: &str, files: Vec<&str>) -> String {
     let luas = Regex::new(r"<lua>(.*)</lua>").unwrap();
     let macros = Regex::new(r"<macro>(.*)</macro>").unwrap();
     let blueprints = Regex::new(r"<blueprint>(.*)</blueprint>").unwrap();
+    let netincludes = Regex::new(r"<netinclude>(.*)</netinclude>").unwrap();
+    let downloads = Regex::new(r"<download>(.*)</download>").unwrap();
+    let gits = Regex::new(r"<git>(.*)</git>").unwrap();
+    let deletes = Regex::new(r"<delete>(.*)</delete>").unwrap();
+    let deletefolders = Regex::new(r"<deletefolder>(.*)</deletefolder>").unwrap();
 
     for include in includes
         .captures_iter(target_text.clone().as_str())
@@ -147,87 +40,20 @@ fn render(target: &str, files: Vec<&str>) -> String {
         }
     }
 
-    for setting in settings
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = setting.0;
-        let set = &setting.1[0];
-        let sep = set.split(":").collect::<Vec<&str>>();
-        let file_name = sep[0];
-        let setting_name = sep[1];
-        let include_text = get_json(file_name, setting_name);
-        target_text = target_text.replace(found, &include_text);
-    }
+    target_text = render_with(target_text, settings, get_json, RenderType::Complex);
+    target_text = render_with(target_text, luas, run_lua, RenderType::Simple);
+    target_text = render_with(target_text, base64s, file_to_base64, RenderType::Simple);
+    target_text = render_with(target_text, macros, run_macro, RenderType::Complex);
+    target_text = render_with(target_text, blueprints, run_blueprint, RenderType::Complex);
+    target_text = render_with(target_text, netincludes, get_file, RenderType::Simple);
+    target_text = render_with(target_text, downloads, download_file, RenderType::Complex);
+    target_text = render_with(target_text, gits, download_git, RenderType::Complex);
+    target_text = render_with(target_text, systems, run, RenderType::Simple);
+    target_text = render_with(target_text, deletes, delete, RenderType::Simple);
+    target_text = render_with(target_text, deletefolders, delete_folder, RenderType::Simple);
 
-    for b64 in base64s
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = b64.0;
-        let file_name = &b64.1[0];
-        let include_text = file_to_base64(file_name);
-        target_text = target_text.replace(found, &include_text);
-    }
-
-    // for system in systems
-    //     .captures_iter(target_text.clone().as_str())
-    //     .map(|c| c.extract::<1>())
-    // {
-    //     let found = system.0;
-    //     let command = &system.1[0];
-    //     let include_text = rem_last(run(command));
-    //     target_text = target_text.replace(found, &include_text);
-    // }
-
-    target_text = render_with(target_text, systems, run);
-
-    for lua in luas
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = lua.0;
-        let code = &lua.1[0];
-        let include_text = run_lua(code);
-        target_text = target_text.replace(found, &include_text);
-    }
-
-    for mac in macros
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = mac.0;
-        let set = &mac.1[0];
-        let sep = set.split(":").collect::<Vec<&str>>();
-        let file_name = sep[0];
-        let mut macro_args = "";
-        if sep.len() > 1 {
-            macro_args = sep[1];
-        }
-        let include_text = run_macro(file_name, macro_args);
-        target_text = target_text.replace(found, &include_text);
-    }
-
-    for blueprint in blueprints
-        .captures_iter(target_text.clone().as_str())
-        .map(|c| c.extract::<1>())
-    {
-        let found = blueprint.0;
-        let set = &blueprint.1[0];
-        let sep = set.split(":").collect::<Vec<&str>>();
-        let file_name = sep[0];
-        let blueprint_args = sep[1];
-        let include_text = run_blueprint(file_name, blueprint_args);
-        target_text = target_text.replace(found, &include_text);
-    }
 
     target_text
-}
-
-fn rem_last(value: String) -> String {
-    let mut chars = value.chars();
-    chars.next_back();
-    chars.as_str().to_string()
 }
 
 use clap::{command, Parser};
@@ -242,10 +68,6 @@ struct Args {
 }
 fn main() {
     let args = Args::parse();
-
-    // println!("Rendering, {}!", args.path);
-    // println!("{}", render(args.path.as_str(), [].to_vec()));
-    // println!("{:?}",args.output.unwrap_or("out.txt".into()))
 
     let file_out = args.output.unwrap_or("out.txt".into());
     let text = render(args.path.as_str(), [].to_vec());
